13c6103b04b728aa2b2025b836f1079f521112d0
Otávio Pace <otaviopp8@gmail.com>
2019-04-22 02:06:00 -0300

    scripts: add files with multiple commands to configure tmux and nvim

---
+++ configure_nvim.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.config/nvim
+
+cp init.vim ~/.config/nvim/init.vim
+
+# install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install pip
+yay -Sy python-pip
+
+sudo pip install neovim
+
+sudo pip install pynvim 
