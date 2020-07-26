c95f012dfc1370e45253e6b64169f730fb6f38cb
Otavio Pace <otaviopp8@gmail.com>
2020-07-26 09:52:23 -0300

    make configure scripts executable, with yay check

--- configure_nvim.sh
+++ configure_nvim.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkdir -p ~/.config/nvim
 
 cp init.vim ~/.config/nvim/init.vim
@@ -5,6 +7,9 @@ cp init.vim ~/.config/nvim/init.vim
 # install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# check yay
+sudo yaourt -S yay
 
 # install pip
 yay -Sy python-pip
